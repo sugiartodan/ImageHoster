880c7ca0f8d8fa66fe7b0223e95fbcb740f5e9e5
danielsugiarto <ryreisback@gmail.com>
2020-12-19 16:54:53 -0500

    add new parameter to registerUser method.

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -42,7 +43,7 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
+    public String registerUser(User user, RedirectAttributes redirectAttrs) {
         if(isPasswordSecure(user.getPassword())) {
             userService.registerUser(user);
             return "users/login";
